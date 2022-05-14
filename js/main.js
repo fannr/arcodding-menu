6fffad017c941fa9a416868328a7df80ec61bf21
fannr <fatahnoer398@gmail.com>
2022-05-15 00:36:41 +0100

    change and create with a jquery UI

--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
-$(".arcodding__container").on("click", function () {
-  const getClassIcons = $(this).find("i").hasClass("bi-plus-lg");
-
-  if (getClassIcons) {
-    $(this).find("i").toggleClass("bi-plus-lg bi-dash-lg");
-  } else {
-    $(this).find("i").toggleClass("bi-plus-lg bi-dash-lg");
-  }
-
-  $(this).find(".desc").slideToggle(250);
+$(function () {
+  $("#accordion").accordion({
+    header: "header",
+    active: false,
+    collapsible: true,
+    heightStyle: "content",
+    icons: { header: "ui-icon-plus", activeHeader: "ui-icon-minus" },
+  });
 });
